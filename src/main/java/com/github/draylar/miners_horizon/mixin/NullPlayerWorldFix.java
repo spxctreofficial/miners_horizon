14bd209c637800d30bb8d2817f4a1d8f5c93c602
Draylar <samuelhimes@gmail.com>
2019-05-22 18:55:15 -0500

    null dimension fix

--- src/main/java/com/github/draylar/miners_horizon/mixin/NullPlayerWorldFix.java
+++ src/main/java/com/github/draylar/miners_horizon/mixin/NullPlayerWorldFix.java
@@ -1,35 +1,40 @@
 package com.github.draylar.miners_horizon.mixin;
 
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.PlayerManager;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 /**
  * I made a goof up somehow. It seems like when players used 1.3.1 or 1.4.0 of Miner's Horizon, their NBT dimension was saved as null.
  * When the game loads, they would crash here. This serves as a temporary fix and will be removed in a month or two.
  */
-@Mixin(PlayerManager.class)
+@Mixin(Entity.class)
 public abstract class NullPlayerWorldFix
 {
-    @Shadow @Final private MinecraftServer server;
+    @Shadow public DimensionType dimension;
 
-    @ModifyVariable(method = "onPlayerConnect", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/server/PlayerManager;loadPlayerData(Lnet/minecraft/server/network/ServerPlayerEntity;)Lnet/minecraft/nbt/CompoundTag;"))
-    private ServerPlayerEntity changeWorld(ServerPlayerEntity serverPlayerEntity_1)
+    @Shadow public World world;
+
+    @Shadow public abstract void setPosition(double double_1, double double_2, double double_3);
+
+    @Inject(method = "fromTag", at = @At("RETURN"))
+    private void setDimensionIfNull(final CompoundTag compoundTag, final CallbackInfo info)
     {
-        if(serverPlayerEntity_1.dimension == null)
-            serverPlayerEntity_1.dimension = DimensionType.OVERWORLD;
+        if (compoundTag.containsKey("Dimension"))
+        {
+            DimensionType type = DimensionType.byRawId(compoundTag.getInt("Dimension"));
 
-        // give player fall resistance for 10 seconds so they don't die
-        serverPlayerEntity_1.addPotionEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 20 * 10, 4));
-
-        return serverPlayerEntity_1;
+            if(type == null)
+            {
+                dimension = DimensionType.OVERWORLD;
+                setPosition(world.getSpawnPos().getX(), world.getSpawnPos().getY(), world.getSpawnPos().getZ());
+            }
+        }
     }
 }
